180fec10e3d31966b4939d52fb0baccb2d1d9d96
Jeferson <58449589+calljef@users.noreply.github.com>
2022-11-25 13:27:02 -0300

    A Independência do Teste

--- testes/negocio/GerenciadoraClientesTest_Ex8.java
+++ testes/negocio/GerenciadoraClientesTest_Ex8.java
@@ -11,11 +11,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Classe de teste criada para garantir o funcionamento das principais operações
+ * Classe de teste criada para garantir o funcionamento das principais operaÃ§Ãµes
  * sobre clientes, realizadas pela classe {@link GerenciadoraClientes}.
  * 
- * @author Gustavo Farias
- * @date 21/01/2035 
+ * @jeferson
+ * @date 21/01/2019 
  */
 public class GerenciadoraClientesTest_Ex8 {
 
@@ -26,7 +26,7 @@ public class GerenciadoraClientesTest_Ex8 {
 	@Before
 	public void setUp() {
 	
-		/* ========== Montagem do cenário ========== */
+		/* ========== Montagem do cenÃ¡rio ========== */
 		
 		// criando alguns clientes
 		Cliente cliente01 = new Cliente(idCLiente01, "Gustavo Farias", 31, "gugafarias@gmail.com", 1, true);
@@ -39,9 +39,9 @@ public class GerenciadoraClientesTest_Ex8 {
 		
 		gerClientes = new GerenciadoraClientes(clientesDoBanco);
 	
-		// a) Abriu conexão com o BD? Então...
-		// b) Criou arquivos e pastas aqui? Então...
-		// c) Inseriu clientes fictícios na base de dados especificamente para os testes desta classe? Então...
+		// a) Abriu conexÃ£o com o BD? EntÃ£o...
+		// b) Criou arquivos e pastas aqui? EntÃ£o...
+		// c) Inseriu clientes fictÃ­cios na base de dados especificamente para os testes desta classe? EntÃ£o...
 	}
 
 	@After
@@ -54,35 +54,35 @@ public class GerenciadoraClientesTest_Ex8 {
 	}
 	
 	/**
-	 * Teste básico da pesquisa de um cliente a partir do seu ID.
+	 * Teste bÃ¡sico da pesquisa de um cliente a partir do seu ID.
 	 * 
-	 * @author Gustavo Farias
-	 * @date 21/01/2035
+	 * @jeferson
+	 * @date 21/01/2019
 	 */
 	@Test
 	public void testPesquisaCliente() {
 
-		/* ========== Execução ========== */
+		/* ========== ExecuÃ§Ã£o ========== */
 		Cliente cliente = gerClientes.pesquisaCliente(idCLiente01);
 		
-		/* ========== Verificações ========== */
+		/* ========== VerificaÃ§Ãµes ========== */
 		assertThat(cliente.getId(), is(idCLiente01));
 		
 	}
 	
 	/**
-	 * Teste básico da remoção de um cliente a partir do seu ID.
+	 * Teste bÃ¡sico da remoÃ§Ã£o de um cliente a partir do seu ID.
 	 * 
-	 * @author Gustavo Farias
-	 * @date 21/01/2035
+	 * @jeferson
+	 * @date 21/01/2019
 	 */
 	@Test
 	public void testRemoveCliente() {
 		
-		/* ========== Execução ========== */
+		/* ========== ExecuÃ§Ã£o ========== */
 		boolean clienteRemovido = gerClientes.removeCliente(idCLiente02);
 		
-		/* ========== Verificações ========== */
+		/* ========== VerificaÃ§Ãµes ========== */
 		assertThat(clienteRemovido, is(true));
 		assertThat(gerClientes.getClientesDoBanco().size(), is(1));
 		assertNull(gerClientes.pesquisaCliente(idCLiente02));
@@ -90,4 +90,4 @@ public class GerenciadoraClientesTest_Ex8 {
 	}
 	
 }
-// A Independência do Teste
+// A IndependÃªncia do Teste
